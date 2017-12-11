fd4e58dd39827eafb2011ae4c25037bea21441bc
张君卓 <my_tac@126.com>
2017-12-11 17:25:42 +0800

    :sparkles: 设置模板目录、静态文件目录，创建flash中间件显示通知

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const config = require('config-lite')(__dirname);
+const routes = require('./routes');
+const pkg = require('./package');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('1');
-  next();
-});
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为ejs
+app.set('view engine', 'ejs');
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+// session中间件
+app.use(session({
+  name: config.session.key, // 设置cookie中保存的session id字段名
+  secret: config.session.secret, // 通过设置secret计算hash值并放在cookie中，使产生的signedCookie防篡改
+  resave: true, // 强制更新session
+  saveUninitialized: false, // 设置为false，即使用户未登陆，强制创建一个session
+  cookie: {
+    maxAge: config.session.maxAge, // 过期时间
+  },
+  store: new MongoStore({// 将 session 存储到 mongodb
+    url: config.mongodb, // mongodb 地址
+  }),
+}));
 
-app.use((req, res, next) => {
-  console.log('2');
-  res.status(200).end();
-});
+// flash中间件，用来显示通知
+app.use(flash());
 
-app.listen(3000);
+// 路由
+routes(app);
+
+// 监听端口，启动程序
+app.listen(config.port, () => {
+  console.log(`${pkg.name} listening on port ${config.port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup')),
+  app.use('/signin', require('./signin')),
+  app.use('/signout', require('./signout')),
+  app.use('/comments', require('./comments')),
+  app.use('/posts', require('./posts'));
+};
